0283e39c5a4154d1ff3be7a6b527572892163a4e
unknown <34904814+omoristation@users.noreply.github.com>
2026-02-20 18:20:57 +0900

    first commit

--- model/server.go
+++ model/server.go
@@ -16,11 +16,11 @@ type Server struct {
 
 	Name                   string `json:"name"`
 	UUID                   string `json:"uuid,omitempty" gorm:"unique"`
-	Note                   string `json:"note,omitempty"`        // 管理员可见备注
-	PublicNote             string `json:"public_note,omitempty"` // 公开备注
-	DisplayIndex           int    `json:"display_index"`                          // 展示排序，越大越靠前
-	HideForGuest           bool   `json:"hide_for_guest,omitempty"`               // 对游客隐藏
-	EnableDDNS             bool   `json:"enable_ddns,omitempty"`                  // 启用DDNS
+	Note                   string `json:"note,omitempty"`           // 管理员可见备注
+	PublicNote             string `json:"public_note,omitempty"`    // 公开备注
+	DisplayIndex           int    `json:"display_index"`            // 展示排序，越大越靠前
+	HideForGuest           bool   `json:"hide_for_guest,omitempty"` // 对游客隐藏
+	EnableDDNS             bool   `json:"enable_ddns,omitempty"`    // 启用DDNS
 	//DDNSProfilesRaw        string `gorm:"default:'[]';column:ddns_profiles_raw" json:"-"` //diy
 	DDNSProfilesRaw        string `gorm:"type:longtext"`
 	//OverrideDDNSDomainsRaw string `gorm:"default:'{}';column:override_ddns_domains_raw" json:"-"` //diy
